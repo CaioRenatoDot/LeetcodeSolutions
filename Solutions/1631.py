be12152bc76f1b8433d6558d5bcace982f952008
CaioRenatoDot <caiorenato146@gmail.com>
2025-05-12 17:14:01 -0300

    feat: resolução do leetcode numero 1631, caminho com menor esforço.

--- Solutions/1631.py
+++ Solutions/1631.py
@@ -1,21 +1,25 @@
-class Solution(object):
+import heapq
+
+class Solution:
     def minimumEffortPath(self, heights):
         m, n = len(heights), len(heights[0])
-        dist = [[math.inf] * n for _ in range(m)]
+        dist = [[float('inf')] * n for _ in range(m)]
         dist[0][0] = 0
-        minHeap = [(0, 0, 0)] # distance, row, col
-        DIR = [0, 1, 0, -1, 0]
+
+        minHeap = [(0, 0, 0)]
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
         while minHeap:
-            d, r, c = heappop(minHeap)
-            if d > dist[r][c]: continue  # this is an outdated version -> skip it
-            if r == m - 1 and c == n - 1:
-                return d  # Reach to bottom right
-            
-            for i in range(4):
-                nr, nc = r + DIR[i], c + DIR[i+1]
-                if 0 <= nr < m and 0 <= nc < n:
-                    newDist = max(d, abs(heights[nr][nc] - heights[r][c]))
-                    if dist[nr][nc] > newDist:
-                        dist[nr][nc] = newDist
-                        heappush(minHeap, (dist[nr][nc], nr, nc))
+            effort, x, y = heapq.heappop(minHeap)
+
+            if x == m - 1 and y == n - 1:
+                return effort
+
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+
+                if 0 <= nx < m and 0 <= ny < n:
+                    newEffort = max(effort, abs(heights[nx][ny] - heights[x][y]))
+                    if newEffort < dist[nx][ny]:
+                        dist[nx][ny] = newEffort
+                        heapq.heappush(minHeap, (newEffort, nx, ny))
